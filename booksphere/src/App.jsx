eec03a46ba4c787921b651e889fb34df90e76599
hassaan1shakil <hassaan.shakil1@gmail.com>
2025-02-13 21:21:58 +0500

    Designed basic layout of homepage with navbar and sidebar

--- booksphere/src/App.jsx
+++ booksphere/src/App.jsx
@@ -1,7 +1,7 @@
 import Login from "./pages/Login/Login"
 import Signup from "./pages/Signup/Signup"
 import Home from "./pages/Home/Home"
-import Profile from "./pages/Profile/Profile"
+import Homepage from "./pages/Homepage/Homepage"
 import {createBrowserRouter, RouterProvider} from "react-router-dom"
 
 export default function App() {
@@ -21,8 +21,8 @@ export default function App() {
         element:<Signup/>,
       },
       {
-        path:"/profile",
-        element:<Profile/>,
+        path:"/homepage",
+        element:<Homepage/>,
       },
     ]
   )
