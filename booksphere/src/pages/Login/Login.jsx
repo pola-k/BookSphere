2966cd0cd84cae0d45234fef8b7331e8de92dab0
pola-k <l226585@lhr.nu.edu.pk>
2025-02-14 19:20:52 +0500

    Fixed Login and Signup Page Design and Included Color Variables

--- booksphere/src/pages/Login/Login.jsx
+++ booksphere/src/pages/Login/Login.jsx
@@ -5,25 +5,26 @@ export default function Login()
 {
     return (
         <>
-            <div className="container">
-                <div className="left-container">
-                </div>
-                <div className="right-container">
-                    <div className="right-header">
-                        <img src="./src/images/logo.png" alt="Logo" />
-                        <h1>Log In to BookSphere</h1>
+            <div className="login-container">
+                <div className="main-container">
+                    <div className="login-left-container">
                     </div>
-                    <p>Welcome back! Pick up where you left off, share your thoughts on books, and explore new literary adventures.
+                    <div className="login-right-container">
+                        <div className="login-right-header">
+                            <img src="./src/images/logo.png" alt="Logo" />
+                            <h1 className>Login to BookSphere</h1>
+                        </div>
+                        <p>Welcome back! Pick up where you left off, share your thoughts on books, and explore new literary adventures.
                          Enter your credentials to continue.</p>
-                    <form className='form'>
-                        <input className='input-field' type="email" placeholder="Email" id="email" name='email' required/>
-                        <input className='input-field' type="password" placeholder="Password" id="password" name='password' required/>
-                        <button className='submit-btn' type="submit">Sign up</button>
-                    </form>
-                    <p>Are you new to BookSphere? <Link to="/signup">Sign Up</Link></p>
+                        <form className='login-form'>
+                            <input className='login-input-field' type="email" placeholder="Email" id="email" name='email' required/>
+                            <input className='login-input-field' type="password" placeholder="Password" id="password" name='password' required/>
+                            <button className='login-submit-btn' type="submit">Login</button>
+                        </form>
+                        <p>Are you new to BookSphere? <Link to="/signup">Sign Up</Link></p>
+                    </div>
                 </div>
             </div>
-
         </>
     )
 }
