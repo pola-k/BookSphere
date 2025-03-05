19dcd5c62149a19d30ae8b23f8963359b8597561
hassaan1shakil <hassaan.shakil1@gmail.com>
2025-03-05 17:07:49 +0500

    Recommendations Tab Complete

--- booksphere/src/pages/Homepage/Homepage.jsx
+++ booksphere/src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,7 @@
-import { Link } from 'react-router-dom'
 import Navbar from '../../components/navbar'
 import Sidebar from '../../components/sidebar'
 import HomeFeed from '../../components/home-feed'
+import RecommendationsFeed from '../../components/recommendations-feed'
 
 export default function Homepage()
 {
@@ -11,18 +11,18 @@ export default function Homepage()
             <Navbar/>
 
             {/* Main Block of Left Sidebar + Content + Recommendations */}
-            <div className='grid grid-cols-[20%_20%_35%_25%] border-1' style={{backgroundColor: "var(--bgcolordark)", borderColor: "var(--bordercolor)"}}>
+            <div className='grid grid-cols-[20%_20%_35%_25%] border-1 border-[var(--bordercolor)]'>
 
                 <div className='col-span-1 overflow-y-auto'>
                     <Sidebar/>
                 </div>
 
-                <div className='col-span-2 h-full overflow-y-auto border-1 px-[3vw] py-[4vh] text-black text-center text-2xl' style={{borderColor: "var(--bordercolor)"}}>
+                <div className='col-span-2 h-full overflow-y-auto border-1 px-[3vw] py-[4vh] text-2xl border-[var(--bordercolor)]'>
                     <HomeFeed/>
                 </div>
 
-                <div className='col-span-1 text-center text-[1vw] border-1' style={{borderColor: "var(--bordercolor)"}}>
-                    Recommendations..
+                <div className='col-span-1 border-1 px-[1.5vw] py-[3vh] h-full overflow-y-auto border-[var(--bordercolor)]'>
+                    <RecommendationsFeed/>
                 </div>
 
             </div>
