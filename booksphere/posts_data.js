c6c860d4c4d5ac25d8d8900c29ef732db2cddab5
hassaan1shakil <hassaan.shakil1@gmail.com>
2025-03-08 09:32:07 +0500

    Comments View Completed

---
+++ booksphere/posts_data.js
@@ -0,0 +1,141 @@
+export const posts = [
+    {
+        id: 1,
+        user: "hassaan",
+        date: "2025-02-18T14:30:00Z",
+        flair: "Question",
+        title: "How to learn machine learning?",
+        media: [],
+        description: "What is a good roadmap to getiing started with ML in order to land a job quickly?",
+        likes: 3,
+        comments: [
+            {
+                id: 1,
+                user: "abdullah",
+                date: "2024-01-18T14:32:00Z",
+                text: "Andrew Ng course on Coursera",
+                reply_id: 0,
+                likes: 2,
+                replies: []
+            },
+            {
+                id: 2,
+                user: "rafay",
+                date: "2024-01-18T14:32:00Z",
+                text: "Andrei Karpathy + 1Blue3Brown videos on Youtube are good supplements to that too",
+                reply_id: 1,
+                likes: 4,
+                replies: []
+            },
+            {
+                id: 3,
+                user: "sameer",
+                date: "2024-01-18T14:32:00Z",
+                text: "Learn the fundamentals from anywhere and implement the code yourself",
+                reply_id: 0,
+                likes: 8,
+                replies: []
+            },
+        ]
+    },
+
+    {
+        id: 2,
+        user: "hassaan",
+        date: "2024-01-18T14:30:00Z",
+        flair: "Question",
+        title: "How to learn machine learning?",
+        media: [],
+        description: "What is a good roadmap to getting started with ML in order to land a job quickly?",
+        likes: 3,
+        comments: [
+            {
+                id: 1,
+                user: "hassaan",
+                date: "2024-01-18T14:32:00Z",
+                text: "Andrew Ng's course on Coursera",
+                reply_id: 0,
+                likes: 2,
+                replies: [
+                    {
+                        id: 2,
+                        user: "alex",
+                        date: "2025-02-02T14:30:00Z",
+                        text: "Andrei Karpathy + 3Blue1Brown videos on YouTube are good supplements to that too",
+                        reply_id: 1,
+                        likes: 4,
+                        replies: [
+                            {
+                                id: 3,
+                                user: "sarah",
+                                date: "2025-02-02T14:30:00Z",
+                                text: "Learn the fundamentals from anywhere and implement the code yourself",
+                                reply_id: 1,
+                                likes: 8
+                            }
+                        ]
+                    },
+                    {
+                        id: 3,
+                        user: "sarah",
+                        date: "2025-02-02T14:30:00Z",
+                        text: "Learn the fundamentals from anywhere and implement the code yourself",
+                        reply_id: 1,
+                        likes: 8
+                    }
+                ]
+            },
+            {
+                id: 4,
+                user: "james",
+                date: "2024-01-18T14:35:00Z",
+                text: "I also recommend the 'Hands-on Machine Learning' book by Aurélien Géron",
+                reply_id: 0,
+                likes: 5,
+                replies: []
+            },
+            {
+                id: 5,
+                user: "sophia",
+                date: "2024-01-18T14:40:00Z",
+                text: "Math and linear algebra are essential, check out Gilbert Strang's lectures.",
+                reply_id: 0,
+                likes: 7,
+                replies: [
+                    {
+                        id: 6,
+                        user: "michael",
+                        date: "2025-02-02T14:30:00Z",
+                        text: "Yes! Understanding eigenvalues/vectors makes ML concepts much clearer.",
+                        reply_id: 5,
+                        likes: 3
+                    }
+                ]
+            }
+        ]
+    },
+
+    {
+        id: 3,
+        user: "hassaan",
+        date: "2025-02-07T14:30:00Z",
+        flair: "Question",
+        title: "How to learn machine learning?",
+        media: ["/src/images/harry-potter-deathly-hallows.jpeg", "/src/images/fire.jpg"],
+        description: "What is a good roadmap to getiing started with ML in order to land a job quickly?",
+        likes: 3,
+        comments: [],
+    },
+
+    {
+        id: 4,
+        user: "hassaan",
+        date: "2025-02-02T14:30:00Z",
+        flair: "Question",
+        title: "How to learn machine learning?",
+        media: ["/src/images/fire.jpg",],
+        description: "What is a good roadmap to getiing started with ML in order to land a job quickly?",
+        likes: 3,
+        comments: [],
+    },
+]
---
+++ booksphere/src/components/comment.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import { formatDistanceToNow } from "date-fns";
+import { toZonedTime } from "date-fns-tz";
+import { Dot } from "lucide-react";
+import message_bubble from '../images/speech-bubble.png';
+import CommentBox from "./comment-box";
+
+export default function Comment({ comment, depth }) {
+
+    // rdeal with very long chain of replies by paging
+
+    const [liked, setLiked] = useState(false);
+    const [reply, setReply] = useState(false);
+
+    function formatDate(date) {
+        const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone; // Get user's time zone
+        const zonedDate = toZonedTime(date, userTimeZone); // Convert UTC to user's timezone
+        let difference = formatDistanceToNow(zonedDate, { addSuffix: true });
+
+        difference = difference.replace(/^about\s/, "");
+        return difference;
+    }
+
+    return (
+
+        <div className="rounded-2xl text-[var(--postcolor)]">
+            {/* make 'px' or 'w' dynamic so cater the chaining of comments */}
+            <div className="flex flex-col gap-[0.85vh] items-start w-full" style={{ paddingLeft: `${depth === 1 ? depth * 1.15 : depth * 1.25}vw` }}>
+
+                {/* Username + Timestamp*/}
+                <div className="flex w-full items-center justify-between text-[0.80vw]">
+
+                    <div className="flex gap-[0.15vw] items-center">
+                        <p className="font-semibold">{comment.user}</p>
+                        <Dot />
+                        <p>{formatDate(comment.date)}</p>
+                    </div>
+
+                </div>
+
+                {/* Comment */}
+                <div className="text-[0.9vw] text-left">
+                    {comment.text}
+                </div>
+
+                {/* Icons */}
+                <div className="flex items-center justify-start gap-[1.15vw] pt-[0.5vh]">
+
+                    {/* Like */}
+                    <div className="flex items-center gap-[0.2vw] text-[0.9vw] font-bold cursor-pointer  transition-transform duration-300 ease-in-out hover:-translate-y-[0.3vh] text-[var(--navbarcolor)]" onClick={() => setLiked(!liked)}>
+
+                        <svg
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-[1vw] transition-all duration-100"
+                            fill={liked ? "red" : "none"} // Toggle fill color
+                            stroke="red" // Keep red stroke for outline effect
+                            strokeWidth="3" // Make outline more visible
+                        >
+                            <path
+                                id="primary"
+                                d="M20.28,4.74a5.82,5.82,0,0,0-8.28,0,5.82,5.82,0,0,0-8.28,0,5.94,5.94,0,0,0,0,8.34l7.57,7.62a1,1,0,0,0,1.42,0l7.57-7.62a5.91,5.91,0,0,0,0-8.34Z"
+                            />
+                        </svg>
+
+                        {comment.likes}
+
+                    </div>
+
+                    {/* Reply */}
+
+                    <div className="flex gap-[0.5vw] transition-transform duration-100 ease-in-out hover:-translate-y-[0.3vh] text-[var(--navbarcolor)] cursor-pointer" onClick={() => setReply(!reply)}>
+                        <img src={message_bubble} alt="" className="h-[1vw]" />
+                        <p className="text-[0.9vw]">Reply</p>
+                    </div>
+
+                    {/* configure properly to send the reply/comment and parent id and maybe depth too*/}
+                    {reply && <CommentBox parent_id={comment.id}/> }
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
